8c7c708fb5405d58e0de5e11dbf39fae40730dee
Jon Robison <narfman0@gmail.com>
2019-03-22 11:35:54 +0000

    -Make easier api for questions/statements

--- fizzure/main.py
+++ fizzure/main.py
@@ -5,6 +5,8 @@ from fizzure import saves
 from fizzure.controller import Controller
 from fizzure.models import Run, Segment
 
+COLUMN_WIDTH = 32
+
 
 def main(stdscr, activator=lambda: True):
     controller = Controller()
@@ -16,18 +18,7 @@ def main(stdscr, activator=lambda: True):
 
 
 def update(stdscr, controller):
-    draw_index = 0
-    if controller.run:
-        if controller.run.game:
-            stdscr.addstr(draw_index, 0, f"Game: {controller.run.game}")
-            draw_index += 1
-        if controller.run.category:
-            stdscr.addstr(draw_index, 0, f"Category: {controller.run.category}")
-            draw_index += 1
-    if controller.active:
-        stdscr.addstr(draw_index, 0, f"Time: {controller.elapsed_time:.3f}")
-    else:
-        stdscr.addstr(draw_index, 0, f"Not started!")
+    draw(stdscr, controller)
     stdscr.refresh()
     controller.update()
     try:
@@ -36,6 +27,26 @@ def update(stdscr, controller):
     except Exception:
         pass  # no input
     time.sleep(0.001)
+
+
+def draw(stdscr, controller):
+    y = 0
+    if controller.run:
+        if controller.run.game:
+            declare(stdscr, "Game:", y=y)
+            declare(stdscr, controller.run.game, y=y, x=COLUMN_WIDTH)
+            y += 1
+        if controller.run.category:
+            declare(stdscr, "Category:", y=y, x=COLUMN_WIDTH)
+            declare(stdscr, controller.run.category, y=y)
+            y += 2
+        for segment in controller.run.segments:
+            declare(stdscr, segment.name, y=y)
+            y += 1
+    if controller.active:
+        declare(stdscr, f"Time: {controller.elapsed_time:.3f}", y=y)
+    else:
+        declare(stdscr, "Not started!", y=y)
 
 
 def handle_input(stdscr, controller, key):
@@ -53,17 +64,15 @@ def init(stdscr):
     stdscr.timeout(-1)
     curses.echo()
     if saves.does_default_exist():
-        stdscr.addstr(0, 0, "Use default? (yN)          ")
-        response = stdscr.getstr()
-        if response.decode("utf-8") == "y":
+        response = ask_question_bool(stdscr, "Use default?", y=0)
+        if response:
             curses.noecho()
             stdscr.timeout(0)
             return saves.load_default()
     run = init_new_run(stdscr)
     stdscr.clear()
-    stdscr.addstr(0, 0, "Save as default? (yN)      ")
-    response = stdscr.getstr()
-    if response.decode("utf-8") == "y":
+    response = ask_question_bool(stdscr, "Save as default?", y=0)
+    if response:
         saves.save_default(run)
     curses.noecho()
     stdscr.timeout(0)
@@ -71,19 +80,28 @@ def init(stdscr):
 
 
 def init_new_run(stdscr):
-    stdscr.addstr(1, 0, "What is the game name?     ")
-    game = stdscr.getstr().decode("utf-8")
-    stdscr.addstr(2, 0, "What is the category name? ")
-    category = stdscr.getstr().decode("utf-8")
-    stdscr.addstr(3, 0, "Initialize segments? (yN)  ")
+    game = ask_question(stdscr, "What is the game name?", y=0)
+    category = ask_question(stdscr, "What is the category name?", y=1)
+    init_segments = ask_question_bool(stdscr, "Initialize segments?", y=2)
+    y = 3
     segments = []
-    response = stdscr.getstr().lower()
-    draw_index = 4
-    if response.decode("utf-8") == "y":
+    if init_segments:
         segment = True
         while segment:
-            stdscr.addstr(draw_index, 0, "Segment name:              ")
-            draw_index += 1
-            segment = stdscr.getstr().decode("utf-8")
+            segment = ask_question(stdscr, "Segment name:", y=y)
+            y += 1
             segments.append(Segment(name=segment))
     return Run(segments=segments, game=game, category=category)
+
+
+def declare(stdscr, message, y=0, x=0, column_width=COLUMN_WIDTH):
+    stdscr.addstr(y, x, f"{message}".ljust(column_width))
+
+
+def ask_question_bool(stdscr, message, y=0, x=0):
+    return ask_question(stdscr, f"{message} (yN)", y=y, x=x) == "y"
+
+
+def ask_question(stdscr, message, y=0, x=0, column_width=COLUMN_WIDTH):
+    stdscr.addstr(y, x, f"{message}".ljust(column_width))
+    return stdscr.getstr().decode("utf-8")
